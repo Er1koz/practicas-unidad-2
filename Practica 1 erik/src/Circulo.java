33aeae434defb98f27a07b835a092c16453ac0bb
Er1koz <162468865+Er1koz@users.noreply.github.com>
2024-05-20 20:03:24 -0600

    Add files via upload

---
+++ Practica 1 erik/src/Circulo.java
@@ -0,0 +1,19 @@
+//erik azael rios amador
+public class Circulo extends Figura{
+    double radio;
+    public Circulo(String nombre, double radio){
+        super(nombre, radio);
+        this.radio = radio;
+        this.nombre = nombre;
+    }
+    public Circulo(){
+        super();
+        System.out.println("Se creo un circulo");
+    }
+    public void area(){
+        double area = Math.PI * Math.pow(radio, 2);
+        System.out.println("El area del circulo es: "+area);
+    }
+
+
+}
