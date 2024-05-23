2d860333c95b4e6a9316dbfa4e565990bad0c0d9
Er1koz <162468865+Er1koz@users.noreply.github.com>
2024-05-23 17:18:57 -0600

    Add files via upload

---
+++ practica 8 erik rios/src/interfaces erik/Relaciones.java
@@ -0,0 +1,9 @@
+//erik azael rios amador
+package ejemplointerfaces;
+
+public interface Relaciones {
+    boolean esMayorQue (Relaciones a);
+    boolean esMenorQue (Relaciones a);
+    boolean esIgualQue (Relaciones a);
+}
+
